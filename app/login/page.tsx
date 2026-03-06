95d0a91fd4e7e06965e85f09889f7a5171590689
SneauxGirl <heatherh@liyudev.com>
2026-03-05 23:14:25 -0800

    Initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { useState } from "react";
+import styles from "./Login.module.scss";
+import { PiXLogoBold } from "react-icons/pi";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebaseConfig";
+import Link from "next/link";
+import { GiToaster } from "react-icons/gi";
+
+export const LoginPage = () => {
+    const [email, setEmail] = useState<string>("testing@fakeemail.com"); //clear after testing
+    const [password, setPassword] = useState<string>("test12345"); //clear after testing
+    const [error, setError] = useState<string | null>(null);
+    const [showToast, setShowToast] = useState<boolean>(false);
+    const router = useRouter();
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError(null);
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            setShowToast(true);
+            router.push("/login");
+        } catch(error) {
+            setError("Failed to login. Please check your email and password.");
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.loginBox}>
+                <div className={styles.logo}>
+                    <PiXLogoBold className={`${styles.icon} ${styles.logo}`} />
+                </div>
+                <h1 className={styles.header}>Sign in to X</h1>
+                {error && <div className={styles.error}>{error}</div>}
+                <form onSubmit={handleLogin} className={styles.form}>
+                    <input
+                        type="email"
+                        placeholder="Phone, email address, or username"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className={styles.input}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className={styles.input}
+                    />
+                    <button
+                        type="submit"
+                        className={styles.button}
+                        disabled={!email || !password}
+                    >
+                        Next
+                    </button>
+                </form>
+                <Link href="#forgotPassword" className={styles.forgotPassword}>Forgot password?</Link>
+                <div className={styles.signupLink}>
+                    Don't have an account? <Link href="/signup">Sign up</Link>
+                </div>
+                {showToast && (
+                    <Toast //link once component is created
+                    message="Successfully signed in"
+                    onClose={() => setShowToast(false)}
+                    />
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ app/signup/page.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import React from 'react';
+import styles from './Signup.module.scss';
+import { PiXLogoBold } from 'react-icons/pi';
+import { useState } from 'react';
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { auth, db } from '@/lib/firebaseConfig';
+
+export const SignUpPage: React.FC = () => {
+
+    const [fullName, setFullName] = useState("");
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("")
+    const [error, setError] = useState<string>("");
+    const [success, setSuccess] = useState<string>(""); // Success message
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+    const router = useRouter();
+
+    const validateForm = (): boolean => {
+        if (!email || !password || !fullName || !username) {
+            setError('All fields are required')
+        }
+        
+        if (password.length < 6) {
+            setError('Password must be at least 6 characters long');
+            return false;
+        }
+        setError("");
+        return true;
+    }
+
+    const handleSignUp = async (event: React.FormEvent) => {
+        event.preventDefault();
+        if (!validateForm()) return;
+
+        setIsSubmitting(true);
+
+        try {
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password,
+        );
+        const user = userCredential.user;
+
+        await updateProfile(user, {
+        displayName: fullName,
+      });
+
+      await setDoc(doc(db, "users", user.uid), {
+        fullName,
+        username,
+        email: user.email,
+        createdAt: new Date(),
+      });
+
+      setSuccess("Account created successfully! Redirecting...");
+      setTimeout(() => {
+        router.push("/login");
+      }, 2000); // Redirect after 2 seconds
+    } catch (error: any) {
+      if (error.code === "auth/email-already-in-use") {
+        setError("An account with this email already exists.");
+      } else {
+        setError("Failed to create an account. Please try again.");
+      }
+      console.error(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+
+    return(
+        <section className={styles.signUpContainer}>
+            <div className={styles.logoContainer}>
+                <PiXLogoBold className={styles.logo} />
+            </div>
+            <h1 className={styles.title}>Join X Today</h1>
+            <form onSubmit={handleSignUp} className={styles.form}>
+                <input className={styles.input} type="text" placeholder='Full Name' value={fullName} onChange={(event) => setFullName(event.target.value)} />
+                <input className={styles.input} type="text" placeholder='Username' value={username} onChange={(event) => setUsername(event.target.value)} />
+                <input className={styles.input} type="email" placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)} />
+                <input className={styles.input} type="password" placeholder='Password' value={password} onChange={(event) => setPassword(event.target.value)} />
+                {error && <p className={styles.error}>{error}</p>}
+                {success && <p className={styles.success}>{success}</p>}
+                <button type='submit' className={styles.button} disabled={isSubmitting}>
+                    {isSubmitting ? "Signing Up..." : "Create account"}
+                </button>
+            </form>
+            <p className={styles.footerText}>
+                By signing up, you agree to the {" "}
+                <Link href='#termsofservice' className={styles.link}>
+                    Terms of Service
+                </Link>{" "}
+                and {" "}
+                <Link href='#privacypolicy' className={styles.link}>
+                    Privacy Policy
+                    </Link>
+                    , including {" "}
+                    <Link href='#cookieuse' className={styles.link}>
+                        Cookie Use
+                    </Link>
+                    .
+            </p>
+            <p className={styles.footerText}>
+                Have an account already?{" "}
+                <Link href="/login" className={styles.link}>
+                Log in</Link>
+            </p>
+        </section>
+    )
+}
+
+export default SignUpPage;
