d5ac49f2a2299a0015578cc0a0b109a03d737653
SneauxGirl <heatherh@liyudev.com>
2026-03-06 21:02:20 -0800

    Complete Sidebar and Right Sidebar

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
 ' use client';
 
+import Sidebar from '../Sidebar';
 import RightSidebar from '../RightSidebar';
 import styles from './HomePage.module.scss'
 
 const HomePage = () => {
     return (
         <div className={styles.container}>
-            <h1>HOME PAGE</h1>
-            {/* Sidebar */}
+            <Sidebar avatarUrl={'https://randomuser.me/api/portraits/men/9.jpg'}/>
             {/* MainContent */}
             <RightSidebar />
         </div>
